eefeb00491268a740f05b6541ba5dd705bae1ef3
pc <pc@DESKTOP-MID8A7L.home>
2022-09-20 15:38:14 +0300

    1st

---
+++ src/miniProjeAsalSayı/Main.java
@@ -0,0 +1,26 @@
+package miniProjeAsalSayı;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number=1;
+		boolean isPrime=true;
+		
+		if(number==1) {System.out.println("Asal Değildir");
+		return;
+		}
+		
+		
+		if(number<1) {System.out.println("Geçersiz Sayı");}
+		
+	for(int i=2;i<number;i++)
+	if(number%i==0) {isPrime=false;}
+	
+	if(isPrime) {
+	System.out.println("Asaldır");}
+	else {System.out.println("Asal Değildir");
+
+	}
+
+}
+}
